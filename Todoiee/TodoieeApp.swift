24f2d545581b58eefa44d659ff362248a1ecf2d3
kitamuraakihiko <kitamuraakihiko@icloud.com>
2024-01-27 23:17:42 +0900

    Initial Commit

---
+++ Todoiee/TodoieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoieeApp.swift
+//  Todoiee
+//
+//  Created by Kitamura Akihiko on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct TodoieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
